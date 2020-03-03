c0b569d657ca2173f03572775f10132619a4fa26
ivohb <ivohb.me@gmail.com>
2020-03-03 14:43:38 -0300

    projeto criado

---
+++ src/main/java/com/ivohb/wsgnic/WsgnicApplication.java
@@ -0,0 +1,13 @@
+package com.ivohb.wsgnic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsgnicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsgnicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivohb/wsgnic/WsgnicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivohb.wsgnic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsgnicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
